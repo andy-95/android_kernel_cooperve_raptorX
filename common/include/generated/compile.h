803cce49eb300b1ce0f607dd95b9ce9e6a0e62a9
Lopicl <lopicl.00@gmail.com>
2013-07-02 15:51:51 +0200

    update:add lzma compress to config, add ext4 support

--- common/include/generated/compile.h
+++ common/include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 15 */
+/* This file is auto generated, version 17 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#15 PREEMPT Mon Jul 1 12:03:55 CEST 2013"
-#define LINUX_COMPILE_TIME "12:03:55"
+#define UTS_VERSION "#17 PREEMPT Tue Jul 2 11:07:03 CEST 2013"
+#define LINUX_COMPILE_TIME "11:07:03"
 #define LINUX_COMPILE_BY "matteo"
 #define LINUX_COMPILE_HOST "thunderkernel-PC"
 #define LINUX_COMPILE_DOMAIN "(none)"
